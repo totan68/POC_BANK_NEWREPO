727408079d09b3241f72e8edf2fdbd0d9a97d7c5
totan68 <164209696+totan68@users.noreply.github.com>
2024-08-06 13:08:38 +0530

    Initial Commit

---
+++ POC_BANK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC_BANK
+//
+//  Created by Totan Hazra on 06/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POC_BANK/POC_BANKApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_BANKApp.swift
+//  POC_BANK
+//
+//  Created by Totan Hazra on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct POC_BANKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
